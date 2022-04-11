9a43b3544b496b211a666bbccbb02f7f40a1581a
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-04-11 11:26:45 -0700

    updated

--- pages/components/MainBody/MainBody.js
+++ pages/components/MainBody/MainBody.js
@@ -6,37 +6,26 @@ function MainBody() {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Moonlight Beach Volleyball Webcam</h1>
-      {/* <GoogleAd /> */}
 
       <p className={styles.description}>We will be live soon! Stay tuned!</p>
 
-      <div className={styles.grid}>
-        <a
-          href="https://instagram.com/best.of.encinitas?igshid=YmMyMTA2M2Y=
+      <a
+        href="https://instagram.com/best.of.encinitas?igshid=YmMyMTA2M2Y=
 https://instagram.com/best.of.encinitas?igshid=YmMyMTA2M2Y="
-          className={styles.card}
-        >
-          <p>Want to know what else is happening in Encinitas? &rarr;</p>
-          <Image
-            src="/BestOfEncinitasLogo.png"
-            alt="BestOfEncinitas"
-            width="100"
-            height="75"
-          />
-        </a>
+        className={styles.card}
+      >
+        <p>Want to know what else is happening in Encinitas? &rarr;</p>
+        <Image
+          src="/BestOfEncinitasLogo.png"
+          alt="BestOfEncinitas"
+          width="100"
+          height="75"
+        />
+      </a>
 
-        {/* <a href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a> */}
-
-        {/* <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a> */}
+      <div className={styles.card}></div>
+      <div style={{ width: 250 }}>
+        <GoogleAd />
       </div>
     </main>
   );
