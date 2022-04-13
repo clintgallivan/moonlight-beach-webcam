54f0bc410e7e1979cd9c637a9b96565a8fdad1e5
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-04-13 16:42:58 -0700

    updated flex

--- pages/components/VideoStream/VideoStream.js
+++ pages/components/VideoStream/VideoStream.js
@@ -5,45 +5,49 @@ import ReactPlayer from 'react-player';
 import { BsPlayBtn } from 'react-icons/bs';
 import styles from './VideoStream.module.scss';
 import { Button } from 'react-bootstrap';
+import { Spinner } from 'react-bootstrap';
+import useWindowSize from '../../../hooks/useWindowSize';
 
 function VideoStream() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const size = useWindowSize();
 
   return !isPlaying ? (
-    <div className={styles.buttonContainer}>
+    <div
+      className={styles.buttonContainer}
+      style={{
+        width: size.width / 1.5,
+        height: ((size.width / 1.5) * 2) / 4,
+      }}
+    >
       <Button onClick={() => setIsPlaying(true)}>
         <BsPlayBtn size={70} className={styles.playButtonIcon} />
       </Button>
     </div>
   ) : (
-    <iframe
-      width="560"
-      height="315"
-      // src="https://www.youtube.com/embed/vF9QRJwJXJk"
-      // src="http://127.0.0.1:5000/"
-      // src="rtsp://username:password@174.65.33.32:8554/profile0"
-      src="https://enigmatic-hollows-62142.herokuapp.com"
-      title="YouTube video player"
-      frameBorder="0"
-      // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    ></iframe>
+    <>
+      {!isLoaded ? (
+        <>
+          <Spinner animation="border" style={{ marginTop: 50 }} />
+          <h2>Bare with us please :) We are a small business</h2>
+        </>
+      ) : (
+        <></>
+      )}
+      <iframe
+        width={size.width / 1.5}
+        height={((size.width / 1.5) * 4) / 3}
+        src="https://enigmatic-hollows-62142.herokuapp.com"
+        title="YouTube video player"
+        frameBorder="0"
+        onLoad={() => console.log(setIsLoaded(true))}
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </>
   );
-
-  // return (
-  //   <iframe
-  //     width="560"
-  //     height="315"
-  //     // src="https://www.youtube.com/embed/vF9QRJwJXJk"
-  //     src="http://127.0.0.1:5000/"
-  //     title="YouTube video player"
-  //     frameborder="0"
-  //     // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  //     allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  //     allowfullscreen
-  //   ></iframe>
-  // );
 }
 
 export default VideoStream;
